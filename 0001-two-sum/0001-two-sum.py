4163983f1bb6717fb79469de7d0ed1f0fa78affd
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2023-07-26 00:21:13 +0700

    Time: 76 ms (70.16%), Space: 17.6 MB (36.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,12 @@ class Solution:
         hash_table = {}
 
         for i in range(n):
-            complement = target - nums[i]
-            if complement in hash_table:
-                return [hash_table[complement], i]
             hash_table[nums[i]] = i
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hash_table and hash_table[complement] != i:
+                return [i, hash_table[complement]]
         
         return []
         
