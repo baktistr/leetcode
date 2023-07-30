5fb216e44cc30006b98e7cb430076baa4cef10ef
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2023-07-30 16:38:38 +0700

    Time: 109 ms (98.85%), Space: 16.6 MB (66.89%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.py
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.py
@@ -1,8 +1,27 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
 
-        answer=[]
+        # answer=[]
+        # for i in range(len(nums)):
+        #     # answer[i] = nums[nums[i]]
+        #     answer.append(nums[nums[i]])
+        # return answer
+
+        # use meta value to modified existing array,  
+        # because constraint problem : 1 < nums.length < 1000, Use cons = 1001
+        # meta = a + cons_val*b  --> meta = nums[i]
+        # a = meta % cons_val --> a = nums[i]
+        # b = meta / cons_val --> b = nums[nums[i]] % cons_val
+
+        # fill the meta value
+        cons_val = 1001
         for i in range(len(nums)):
-            # answer[i] = nums[nums[i]]
-            answer.append(nums[nums[i]])
-        return answer
+            # a = nums[i]
+            b = nums[nums[i]]%cons_val
+            nums[i] = nums[i] + b*cons_val
+
+        # retrive the real value
+        for i in range(len(nums)):
+            nums[i] = nums[i]//cons_val
+
+        return nums
