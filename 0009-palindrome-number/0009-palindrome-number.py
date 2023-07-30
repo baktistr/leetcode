ab0d911f7c8f6d937537b40af024d90c1c5bfc77
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2023-07-30 23:30:11 +0700

    Time: 64 ms (91.26%), Space: 16.4 MB (41.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
-        string_x = str(x)
-        for i in range(len(string_x)):
-            if string_x[i] == string_x[len(string_x)-1-i]:
-                print(string_x[i])
-                print(string_x[len(string_x)-1-i])
-                continue
-            else :
-                return False
-        return True
+        # string_x = str(x)
+        # for i in range(len(string_x)):
+        #     if string_x[i] == string_x[len(string_x)-1-i]:
+        #         print(string_x[i])
+        #         print(string_x[len(string_x)-1-i])
+        #         continue
+        #     else :
+        #         return False
+        # return True
+
+        return str(x) == str(x)[::-1]
