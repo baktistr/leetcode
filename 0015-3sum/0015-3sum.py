307a02e714a7b6fa046e5a04fcfd47c4a3c8e0f4
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2023-07-31 12:33:18 +0700

    Time: 1084 ms (89.12%), Space: 20.5 MB (41.13%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        result = []
+
+        # use greedy, iterate i, and use two pointer (left & right) for each possible triplet with i
+        # sort the nums, so we can move the two pointers accordingly
+        nums.sort()
+
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums [i-1] : # skip for duplicate
+                continue
+            
+            # two pointers
+            left = i + 1
+            right = len(nums) - 1
+
+            # greedy for two pointer
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0 :# we need to find a bigger value, 
+                    left += 1
+                elif total > 0 :
+                    right -= 1
+                else :
+                    result.append([nums[i], nums[left], nums[right]] )
+
+                    # check another triplet
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    # while left < right and nums[right] == nums[right-1]:
+                    #     right -= 1
+
+                    left += 1
+                    right -= 1
+
+        return result
