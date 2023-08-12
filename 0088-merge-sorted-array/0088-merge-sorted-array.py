471ea71912855f24b4446bf48ba3f8eb678a9582
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2023-08-13 04:02:28 +0700

    Time: 46 ms (76.1%), Space: 16.3 MB (84.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -26,7 +26,7 @@ class Solution:
         p2 = -1 
         p = 1
         """
-        
+        """
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
@@ -41,4 +41,14 @@ class Solution:
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
+        """
 
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else :
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            
+            p -= 1
