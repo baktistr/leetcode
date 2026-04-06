00ea6c4e4e136d306f9931c2fcb432408d788fc3
Bakti Satria Adhityatama <bakti.satria.a@gmail.com>
2026-04-05 20:20:33 -0400

    Time: 0 ms (100%), Space: 19.3 MB (35.86%) - LeetHub

---
+++ Python3/Easy/0027-remove-element/0027-remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        output = []
+        index = 0
+        for num in nums:
+            if num ==val:
+                continue
+            output.append(num)
+        for i in range(len(output)):
+            nums[i] = output[i]
+
+        return len(output)
